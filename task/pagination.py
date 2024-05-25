b222875f9f1947222c5e03823dec8a135e6b3d48
Ghostt op <ahtishamcod123@gmail.com>
2024-05-25 16:10:37 +0500

    checked

--- task/pagination.py
+++ task/pagination.py
@@ -12,7 +12,7 @@ class CustomPagination(PageNumberPagination):
             'count': self.page.paginator.count,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
-            'results': data
+            'results': data #sadsadsad
         })
 
     def get_next_link(self):
